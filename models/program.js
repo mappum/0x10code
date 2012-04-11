47c40db1b98a866b62d93b45cd448e83cda5d803
Jonathan Castello <twisolar@gmail.com>
2012-04-11 03:13:45 -0700

    Refactoring stuff that used getRecent.

--- models/program.js
+++ models/program.js
@@ -2,15 +2,15 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var programSchema = new Schema({
-	'id': {'type': String, 'unique': true},
-    'title': {'type': String, 'default': 'Untitled'},
-    'author': {'type': String, 'default': 'Anonymous'},
+	'id':          {'type': String, 'unique': true},
+    'title':       {'type': String, 'default': 'Untitled'},
+    'author':      {'type': String, 'default': 'Anonymous'},
     'description': {'type': String, 'default': ''},
-    'date': {'type': Date, 'default': Date.now},
-    'code': {'type': String},
-    'views': {'type': Number, 'default': 0},
-    'password': {'type': String, 'default': ''},
-    'fork': {'type': String, 'default': ''}
+    'date':        {'type': Date,   'default': Date.now},
+    'code':        {'type': String},
+    'views':       {'type': Number, 'default': 0},
+    'password':    {'type': String, 'default': ''},
+    'fork':        {'type': String, 'default': ''}
 });
 
 module.exports = mongoose.model('Program', programSchema);
