0a076aac49891cbd3e5462bcf189c688125c2f1a
Matt Bell <matt@safeb.it>
2012-04-27 14:02:58 -0700

    Fixed hardware connecting

--- js/ui.js
+++ js/ui.js
@@ -434,7 +434,8 @@ $(function() {
     
     var driveAdded = false;
     while(devices.length > 0) {
-    	if(Math.random() < (1 / (devices.length + 1)) && !driveAdded) {
+    	if((Math.random() < (1 / (devices.length + 1) || devices.length === 1))
+    	&& !driveAdded) {
     		driveAdded = true;
     		var drive = new DCPU16.HMD2043(cpu);
     		drive.insert(new DCPU16.HMU1440());
